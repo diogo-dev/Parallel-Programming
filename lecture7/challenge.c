b2b837123bb5ec604e84e27d0c24fbf12e1a8dc7
Diogo <diogofelipe.dev@gmail.com>
2025-05-14 23:54:34 -0300

    Challenge proposed in lecture 7

---
+++ lecture7/challenge.c
@@ -0,0 +1,159 @@
+/* Desafio: Modifique o exemplo para realizar a ordenacao sobre o mesmo vetor,
+ou seja, sem usar vetor auxiliar com a copia dos dados. OBS: eh permitido usar
+estruturas auxiliares apenas para controlar a ordenacao */
+
+//  Diogo Felipe Soares da Silva, RA: 124771
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <pthread.h>
+
+#define maxVetor 1000
+
+typedef int tipoVetor[maxVetor];
+
+tipoVetor vetorA, vetorAux;
+
+int iret1, iret2, id1=1, id2=2, tamVet;
+
+int flagTroca = 0; // 0 = livre, 1 = ocupada
+int turno = 1;     // 1 ou 2: quem pode entrar quando há disputa
+
+pthread_t thread1, thread2;
+
+void *selecaoDireta(void *ptr);
+
+void geraVetor(void)
+{   int i;
+
+    for (i=0; i<tamVet; i++) 
+        vetorA[i]=rand()%100;
+       
+}
+
+void mostraVetor(char *nome, tipoVetor v)
+{   int i;
+
+    printf("\n\nVetor %s\n", nome);
+    for (i=0; i<tamVet; i++)
+        printf("%d, ", v[i]);
+    printf("\n");
+
+}
+
+void trocaValores(int *vetor, int id_origem, int id_destino) {
+    int aux = vetor[id_origem];
+    vetor[id_origem] = vetor[id_destino];
+    vetor[id_destino] = aux;
+}
+
+void trava(int id)
+{
+    while (1)
+    {
+        while (flagTroca);  // espera se está ocupado
+        flagTroca = 1;
+        if (turno == id)
+            break;
+        flagTroca = 0;
+        sched_yield();  // cede a CPU, evita busy wait total
+    }
+}
+
+void destrava(int outro_id)
+{
+    turno = outro_id;
+    flagTroca = 0;
+}
+
+void main(void)
+{   
+    int i;
+
+    printf("\nDigite o tamanho do Vetor => ");
+    scanf("%d", &tamVet);
+
+    srand(time(NULL));
+    geraVetor();
+
+    mostraVetor("Vetor A", vetorA);
+
+    iret1 = pthread_create(&thread1, NULL, selecaoDireta, (void *) &id1);
+    iret2 = pthread_create(&thread2, NULL, selecaoDireta, (void *) &id2);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    mostraVetor("Vetor A Ordenado", vetorA);
+
+    printf("\n");
+
+}
+
+/*
+    Problema que eh necessario solucionar quando usamos o mesmo vetor para fazer a ordenacao
+
+    Exemplo do problema:
+    1 - Thread 1 encontra o menor valor na posMenor = 4;
+    2 - Antes de trocar, Thread 2 entra, troca valores em vetorA[4] e sai;
+    3 - Agora posMenor não é mais o menor, mas Thread1 ainda troca posMenor com posDestino, corrompendo a ordenacao
+
+    Solucao: a regiao critica deve conter busca e troca
+*/
+
+
+void *selecaoDireta(void *ptr)
+{   
+    int *id, aux, posDestino, posMenor, posVarredura, posMaior;
+
+    id=(int *)ptr;
+
+    if ((*id)==1)
+    {
+        printf("\n\nThread %d - entrei antes\n", *id);
+
+        for (posDestino=0; posDestino<(tamVet/2); posDestino++)
+        {   
+            // entra na seção critica para buscar e trocar
+            trava(*id);
+
+            posMenor=posDestino;
+            for (posVarredura=posMenor+1; posVarredura<tamVet; posVarredura++)
+            {
+                if (vetorA[posMenor]>vetorA[posVarredura])
+                    posMenor=posVarredura;
+            }
+            
+            trocaValores(vetorA, posMenor, posDestino);
+            destrava(2);
+        }
+        printf("\nThread %d finalizada\n", *id);
+
+    }
+    else
+    {
+        printf("\nThread %d - entrei depois\n", *id);
+
+        for (posDestino=tamVet-1; posDestino>=(tamVet/2); posDestino--)
+        {   
+            trava(*id);
+            
+            posMaior=posDestino;
+            for (posVarredura=posMaior-1; posVarredura>=0; posVarredura--)
+            {
+                if (vetorA[posMaior]<vetorA[posVarredura])
+                    posMaior=posVarredura;
+            }
+            
+            trocaValores(vetorA, posMaior, posDestino);
+            destrava(1);
+        }
+        printf("\nThread %d finalizada\n", *id);
+    }
+
+}
+
+
